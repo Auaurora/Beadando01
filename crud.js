b1192dd8cbef2eb030cbc018eafe0fea5d26413d
Auaurora <161447867+Auaurora@users.noreply.github.com>
2026-04-18 19:45:13 +0200

    Update Javascript

--- crud.js
+++ crud.js
@@ -1,5 +1,26 @@
 var selectedIndex = null;
 var array1 = new Array();
+array1.push({"az":"1","nev":"francia","felveheto":"16","min":"140"});
+array1.push({"az":"2","nev":"angol","felveheto":"30","min":"150"});
+printArray();
+function printArray(){
+    var table = document.getElementById("kepzeslista").getElementsByTagName('tbody')[0];
+    table.innerHTML="";
+    var newRow;
+    for (i = 0; i < array1.length; i++) {
+        newRow = table.insertRow(table.length);
+        cell1 = newRow.insertCell(0);
+        cell1.innerHTML = array1[i].az;
+        cell2 = newRow.insertCell(1);
+        cell2.innerHTML = array1[i].nev;
+        cell3 = newRow.insertCell(2);
+        cell3.innerHTML = array1[i].felveheto;
+        cell4 = newRow.insertCell(3);
+        cell4.innerHTML = array1[i].min;
+        cell4 = newRow.insertCell(4);
+        cell4.innerHTML = '<a onClick="onEdit('+i+')">Edit</a>' + ' | ' + '<a onClick="onDelete('+i+')">Delete</a>';
+    }
+}
 function onFormSubmit() {
     if (validate()) {
         var formData = readFormData();
@@ -12,56 +33,37 @@ function onFormSubmit() {
 }
 function readFormData() {
     var formData = {};
-    formData["vnev"] = document.getElementById("vnev").value;
-    formData["knev"] = document.getElementById("knev").value;
-    formData["nkod"] = document.getElementById("nkod").value;
-    formData["ido"] = document.getElementById("ido").value;
+    formData["az"] = document.getElementById("az").value;
+    formData["nev"] = document.getElementById("nev").value;
+    formData["felveheto"] = document.getElementById("felveheto").value;
+    formData["min"] = document.getElementById("min").value;
     return formData;
 }
 
 function insertNewRecord(data) {
-    array1[array1.length]= {"vnev":data.vnev,"knev":data.knev,"nkod":data.nkod,"ido":data.ido};
+    array1.push({"az":data.az,"nev":data.nev,"felveheto":data.felveheto,"min":data.min});
     printArray();
 }
 
-function printArray(){
-    var table = document.getElementById("nevsor");
-    table.innerHTML="";
-    var newRow;
-    for (i = 0; i < array1.length; i++) {
-        newRow = table.insertRow(table.length);
-        cell1 = newRow.insertCell(0);
-        cell1.innerHTML = array1[i].vnev;
-        cell2 = newRow.insertCell(1);
-        cell2.innerHTML = array1[i].knev;
-        cell3 = newRow.insertCell(2);
-        cell3.innerHTML = array1[i].nkod;
-        cell4 = newRow.insertCell(3);
-        cell4.innerHTML = array1[i].ido;
-        cell4 = newRow.insertCell(4);
-        cell4.innerHTML = '<a onClick="onEdit('+i+')">Edit |</a>' + '<a onClick="onDelete('+i+')"> Delete</a>';
-    }
-}
-
 function resetForm() {
-    document.getElementById("vnev").value = "";
-    document.getElementById("knev").value = "";
-    document.getElementById("nkod").value = "";
-    document.getElementById("ido").value = "";
+    document.getElementById("az").value = "";
+    document.getElementById("nev").value = "";
+    document.getElementById("felveheto").value = "";
+    document.getElementById("min").value = "";
     selectedIndex=null;
 }
 function onEdit(index) {
-    document.getElementById("vnev").value = array1[index].vnev;
-    document.getElementById("knev").value = array1[index].knev;
-    document.getElementById("nkod").value = array1[index].nkod;
-    document.getElementById("ido").value = array1[index].ido;
+    document.getElementById("az").value = array1[index].az;
+    document.getElementById("nev").value = array1[index].nev;
+    document.getElementById("felveheto").value = array1[index].felveheto;
+    document.getElementById("min").value = array1[index].min;
     selectedIndex=index;
 }
 function updateRecord(formData) {
-    array1[selectedIndex].vnev=formData.vnev;
-    array1[selectedIndex].knev=formData.knev;
-    array1[selectedIndex].nkod=formData.nkod;
-    array1[selectedIndex].ido=formData.ido;
+    array1[selectedIndex].az=formData.az;
+    array1[selectedIndex].nev=formData.nev;
+    array1[selectedIndex].felveheto=formData.felveheto;
+    array1[selectedIndex].min=formData.min;
     printArray();
 }
 function onDelete(index) {
@@ -73,8 +75,8 @@ function onDelete(index) {
 }
 function validate() {
     isValid = true;
-    if (document.getElementById("vnev").value.length == 0 || document.getElementById("knev").value.length == 0 
-    ||document.getElementById("nkod").value.length == 0 || document.getElementById("ido").value.length == 0) {
+    if (document.getElementById("az").value.length == 0 || document.getElementById("nev").value.length == 0 
+    ||document.getElementById("felveheto").value.length == 0 || document.getElementById("min").value.length == 0) {
         isValid = false;
     } else {
         isValid = true;

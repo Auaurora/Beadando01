ffd7b9402d8dd4b5959a2b2aad7ed2fc86472a59
Auaurora <161447867+Auaurora@users.noreply.github.com>
2026-04-25 22:33:06 +0200

    Small adjustments

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$db   = "felveteli";
-$user = "root";
-$pass = "";
+$db   = "hajnalka";
+$user = "hajnalka";
+$pass = "xX12345*";
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=UTF8",$user,$pass,
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
